7d12fe59da82b509a28e49a22c34ee59f3214999
cyrus123live <64929788+cyrus123live@users.noreply.github.com>
2025-03-16 15:44:33 -0700

    Cyrus - added file management

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import wave
+import pandas as pd
+import os 
+import shutil
 
 pygame.mixer.init()
+
+# Get function for files csv/dataframe
+def get_files():
+    os.makedirs("visualizer_runtime_data/audio_files", exist_ok=True)
+    if os.path.isfile("visualizer_runtime_data/files.csv"):
+        files = pd.read_csv("visualizer_runtime_data/files.csv", index_col=0)
+    else:
+        files = pd.DataFrame(columns=["file_path"])
+
+    return files
+
+def set_files(files):
+    files.to_csv("visualizer_runtime_data/files.csv")
 
 # Called by "Play" button
 def play_audio(file_path):
@@ -96,24 +112,55 @@ def visualize_audio(file_path):
 
 # Uploading file
 def upload():
-    file_path = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+    files = get_files()
 
-    # Show and enable the play button after a file is selected
+    # Parse file path input
+    file_path = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+    name = file_path.split("/")[-1].split(".")[0]
+    file_path = shutil.copyfile(file_path, "visualizer_runtime_data/audio_files/" + name + ".wav")
+
+    # Parse song and add to csv's
     if file_path:
-        play_button.config(state=tk.NORMAL)
-        play_button.config(command=lambda: play_audio(file_path))
+        files.loc[name, "file_path"] = file_path
+        print(files)
+
+    # Save modified csv's
+    set_files(files)
+    draw_play_buttons()
+
+def draw_play_buttons():
+    files = get_files()
+    play_buttons = {}
+    for widget in play_buttons_frame.winfo_children():
+        widget.destroy()
+    for i in range(files.size):
+        print(files.iloc[i]["file_path"])
+        play_buttons[i] = tk.Button(play_buttons_frame, text=f"Play {files.index[i]}", command=lambda file_path=files.iloc[i]["file_path"]: play_audio(file_path))
+        play_buttons[i].pack(pady=5)
+
+def close():
+    shutil.rmtree("visualizer_runtime_data") # For now we don't save anything permanently, for testing
+    quit()
 
 # Draw upload / play window
 root = tk.Tk()
 root.title("File Upload and Play with Visualizer")
-root.geometry("300x200")
+root.geometry("500x300")
 
-# Creat buttons within above window
-upload_button = tk.Button(root, text="Upload a file", command=upload)
-upload_button.pack(pady=50)
+# Make quit and upload buttons
+menu_frame = tk.Frame(root)
+upload_button = tk.Button(menu_frame, text="Quit", command=close)
+upload_button.pack(pady=0, side="right")
 
-play_button = tk.Button(root, text="Play", state=tk.DISABLED)  # Initially disabled
-play_button.pack(pady=20)
+upload_button = tk.Button(menu_frame, text="Upload a file", command=upload)
+upload_button.pack(pady=0, side="left")
+
+menu_frame.pack(pady=5)
+
+# Make play buttons
+play_buttons_frame = tk.Frame(root)
+play_buttons_frame.pack(pady=20)
+draw_play_buttons()
 
 # Run the Tkinter event loop
 root.mainloop()
